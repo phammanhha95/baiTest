664787250e7b73808a69e93a32bb4aa1723c08d0
Ha <phammanhha121095@gmail.com>
2019-12-10 08:08:37 +0700

    Initial Commit

---
+++ baiTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  baiTest
+//
+//  Created by Phạm Mạnh Hà on 12/10/19.
+//  Copyright © 2019 Phạm Mạnh Hà. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
